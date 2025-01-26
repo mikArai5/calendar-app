39a1250552de443e35328c06e0f7a38466093b63
荒井美香 <mika8500@yk.commufa.jp>
2025-01-26 20:25:01 +0900

    add changes

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -6,8 +6,8 @@ import interactionPlugin from "@fullcalendar/interaction";
 import { DateSelectArg } from "@fullcalendar/core/index.js";
 import { useRouter } from "next/navigation";
 import { addSchedule } from "@/utils/supabaseFunctions";
-
-
+import { useEffect, useState } from "react";
+import { supabase } from "../../utils/supabase";
 
 interface Event {
     title:string;
@@ -16,8 +16,18 @@ interface Event {
     id: number
 }
 
-
 export default function Calendar() {
+    const [ schedules, setSchedules ] = useState<any>([]);
+
+    useEffect(() => {
+        fetchSchedules();
+    }, []);
+
+    async function fetchSchedules() {
+        const { data } = await supabase.from("calendar").select("*");
+        setSchedules(data);
+    }
+
     const handleDateSelect= async (args: DateSelectArg) => {
         const title = prompt('予定のタイトルを入力してください');
         const content = "test";
@@ -32,7 +42,7 @@ export default function Calendar() {
                 end: args.endStr,
                 allDay: args.allDay,
             })
-            await addSchedule(title, content);
+            await addSchedule(title);
         }
     }
     const router = useRouter();
@@ -41,34 +51,47 @@ export default function Calendar() {
         const slug: string = args.event.startStr;
         router.push( `/events/${slug}?date=${slug}`);
     }
+    
     return (
         <>
         <nav className="flex justify-between mb-12 border-b border-violet-100">
         <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
         </nav>
         <main>
-        <div>
             <div>
-                <FullCalendar
-                    plugins={[dayGridPlugin, interactionPlugin]}
-                    initialView="dayGridMonth"
-                    eventTimeFormat={{
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false, // 24時間表記を強制
-                    }}
-                    events={[
-                    { title: '終日イベント', start: '2024-10-15', end: '2024-10-20' },
-                    { title: '時間イベント', start: '2024-10-25T00:00:00Z' },
-                    { title: '時間イベント', start: '2024-10-25T09:00:00Z' },
-                    { title: 'ハロウィン', start: '2024-10-31' },
-                    ]}
-                    selectable={true}
-                    select={handleDateSelect}
-                    eventClick={handleDetail}
-                />
+                <div>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, interactionPlugin]}
+                        initialView="dayGridMonth"
+                        eventTimeFormat={{
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false, // 24時間表記を強制
+                        }}
+                        events={[
+                        { title: '終日イベント', start: '2024-10-15', end: '2024-10-20' },
+                        { title: '時間イベント', start: '2024-10-25T00:00:00Z' },
+                        { title: '時間イベント', start: '2024-10-25T09:00:00Z' },
+                        { title: 'ハロウィン', start: '2024-10-31' },
+                        ]}
+                        selectable={true}
+                        select={handleDateSelect}
+                        eventClick={handleDetail}
+                    />
+                </div>
+                <div>
+                    {schedules.map((schedule: any)=> (
+                        <div key={schedule.id}>
+                            <p>{schedule.title}</p>
+                            <p>
+                                {schedule.start}
+                                <span>~</span>
+                                {schedule.end}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
         </main>
         </>
     );
--- utils/supabaseFunctions.ts
+++ utils/supabaseFunctions.ts
@@ -1,5 +1,10 @@
 import { supabase } from "./supabase";
 
-export const addSchedule = async (title: string, content: string) => {
-    await supabase.from("calendar").insert({ title: title, content: content});
+export const addSchedule = async (title: string) => {
+    await supabase.from("calendar").insert({ title: title});
 };
+
+export const getAllSchedules = async () => {
+    const schedules = await supabase.from("calendar").select("*");
+    return schedules.data;
+}
