4394391b7b0abf2af547da14c9b998e179a0d7c9
荒井美香 <mika8500@yk.commufa.jp>
2025-02-12 23:14:38 +0900

    add changes

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -20,6 +20,7 @@ export default function Page() {
     const scheduleId = path.split('/').pop() as string;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const [ schedule, setSchedule ] = useState<any>([]);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [ schedules, setSchedules ] = useState<any>([]);
     const [ editSchedule, setEditSchedule ] = useState<EditSchedule>({
         id: scheduleId,
