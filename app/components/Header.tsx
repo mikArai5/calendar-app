d3d24b07d4d975f714355ba5944593015a064091
荒井美香 <mika8500@yk.commufa.jp>
2025-02-22 19:31:07 +0900

    ログアウト機能実装

---
+++ app/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { supabase } from '@/utils/supabase';
+import { useRouter } from 'next/navigation';
+
+export default function Header () {
+
+    const router = useRouter();
+    const Logout = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const { error: logoutError } = await supabase.auth.signOut();
+            if (logoutError) {
+                throw logoutError;
+            }
+            await router.push('/login');
+        } catch {
+            alert('エラーが発生しました')
+        }
+    }
+    return (
+        <div className="flex justify-between mb-12 border-b border-violet-100 p-4">
+        <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
+        <form onSubmit={Logout}>
+            <button type="submit">ログアウト</button>
+        </form>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import Calendar from "@/app/components/Calendar";
+import Header from "./components/Header";
 import './styles/style.css';
 
 export default function Home() {
   return (
     <>
-    <div className="flex justify-between mb-12 border-b border-violet-100 p-4">
-      <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
-    </div>
+    <Header />
     <main>
       <div className="calendar">
         <div className="calendar__inner">
