e1fe9fb362f86ff072f77dc0aba7d9ecaf53b0d4
荒井美香 <mika8500@yk.commufa.jp>
2025-02-22 18:42:07 +0900

    ユーザー情報取得　編集中

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -5,12 +5,8 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { DateSelectArg, EventClickArg } from "@fullcalendar/core/index.js";
 import { useRouter } from "next/navigation";
-import { addSchedule } from "@/utils/supabaseFunctions";
-import { useEffect, useState } from "react";
-import { supabase } from "../../utils/supabase";
-import { PostgrestResponse } from "@supabase/supabase-js";
+import { useState } from "react";
 import '../styles/components/calendar.css';
-import { fetchUserInfo } from "@/app/actions";
 
 type Schedule = {
     id: string;
@@ -20,63 +16,32 @@ type Schedule = {
 }
 type Schedules = Schedule[];
 
-export default function Calendar() {
 
-    const [ schedules, setSchedules ] = useState<Schedules>([]);
+export default function Calendar({allSchedules}) {
 
-    // 予定が追加されたタイミングで全ての予定取得
-    useEffect(() => {
-        fetchSchedules();
-    }, [schedules]);
+    const [ schedules, setSchedules ] = useState<Schedules>(allSchedules);
 
-    // ログインしたユーザーが登録した予定のみ表示
-    async function fetchSchedules() {
-        try {
-            // ログインしているユーザーの情報を取得
-            const user = await fetchUserInfo();
-            if (!user?.id) {
-                console.error('Useridが見つかりません');
-                return;
-            }
-            const userId = user?.id as string;
 
-            // ログインしているユーザーのIDと一致する投稿のみを取得
-            const { data, error }: PostgrestResponse<Schedule> = await supabase
-                .from("calendar")
-                .select("*")
-                .eq('userId', userId);
-    
-            if (error) {
-                console.error('Error fetching schedules:', error);
-                return;
-            }
-    
-            // schedulesを更新
-            setSchedules(data);
-        } catch (err) {
-            console.error('Error fetching schedules:', err);
-        }
-    }
+    // const handleDateSelect= async (args: DateSelectArg) => {
 
-    const handleDateSelect= async (args: DateSelectArg) => {
+    //     // 登録した予定・開始日・終了日の入力値を取得して代入
+    //     const title = prompt('予定のタイトルを入力してください');
+    //     const start = prompt('予定の開始日を入力してください') as string;
+    //     const end = prompt('予定の終了日を入力してください') as string;
+    //     const calendarInstance = args.view.calendar;
+    //     // ログインしているユーザーのIDを取得して代入
+    //     const user = await fetchUserInfo();
+    //     const userId = user?.id as string;
 
-        // 登録した予定・開始日・終了日の入力値を取得して代入
-        const title = prompt('予定のタイトルを入力してください');
-        const start = prompt('予定の開始日を入力してください') as string;
-        const end = prompt('予定の終了日を入力してください') as string;
-        const calendarInstance = args.view.calendar;
-        // ログインしているユーザーのIDを取得して代入
-        const user = await fetchUserInfo();
-        const userId = user?.id as string;
+    //     // 選択状態を解除
+    //     calendarInstance.unselect();
 
-        // 選択状態を解除
-        calendarInstance.unselect();
+    //     // 登録した予定・開始日・終了日・ログインしているユーザーのIDをsupabaseに登録
+    //     if (title) {
+    //         await addSchedule(title, start, end, userId);
+    //     }
+    // }
 
-        // 登録した予定・開始日・終了日・ログインしているユーザーのIDをsupabaseに登録
-        if (title) {
-            await addSchedule(title, start, end, userId);
-        }
-    }
     const router = useRouter();
 
     // 追加・表示した予定を押したら詳細ページに遷移
@@ -85,8 +50,26 @@ export default function Calendar() {
         router.push( `/events/${slug}`);
     }
 
+    const selectDate = async (args: DateSelectArg) => {
+    }
+
     return (
         <>
+        <div className="inputForm">
+            <div className="inputForm__item">
+                <label htmlFor="title">タイトル</label>
+                <input className="schedule__title" id="title" type="text" />
+            </div>
+            <div className="inputForm__item">
+                <label htmlFor="start">開始</label>
+                <input id="start" type="date" />
+            </div>
+            <div className="inputForm__item">
+                <label htmlFor="end">終了</label>
+                <input id="end" type="date" />
+            </div>
+            <button className="addButton">追加</button>
+        </div>
         <FullCalendar
             plugins={[dayGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
@@ -97,7 +80,7 @@ export default function Calendar() {
             }}
             events={schedules}
             selectable={true}
-            select={handleDateSelect}
+            select={selectDate}
             eventClick={handleDetail}
             contentHeight={'auto'}
         />
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,46 @@
-"use client";
 import Calendar from "@/app/components/Calendar";
 import './styles/style.css';
+import { PostgrestResponse } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/server";
+
+type Schedule = {
+  id: string;
+  title: string;
+  start: string;
+  end: string;
+}
+
+const fetchSchedules = async() => {
+  try {
+      // ログインしているユーザーの情報を取得
+
+      const supabase = await createClient();
+      const authData = await supabase.auth.getUser();
+      console.log({'authData': authData});
+
+      if (!user?.id) {
+          console.error('Useridが見つかりません');
+          return;
+      }
+      const userId = user?.id as string;
+
+      // ログインしているユーザーのIDと一致する投稿のみを取得
+      const { data, error }: PostgrestResponse<Schedule> = await supabase
+          .from("calendar")
+          .select("*")
+          .eq('userId', userId);
+      if (error) {
+          console.error('Error fetching schedules:', error);
+          return;
+      }
+
+    } catch (err) {
+        console.error('Error fetching schedules:', err);
+    }
+}
+
+const allSchedules = await fetchSchedules();
+console.log(allSchedules);
 
 export default function Home() {
   return (
@@ -11,7 +51,7 @@ export default function Home() {
     <main>
       <div className="calendar">
         <div className="calendar__inner">
-          <Calendar />
+          <Calendar allSchedules={allSchedules}/>
         </div>
       </div>
     </main>
