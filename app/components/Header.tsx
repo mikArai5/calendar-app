12800457c3f7f85754244268b5a58d20fa270ba2
荒井美香 <mika8500@yk.commufa.jp>
2025-02-23 11:08:31 +0900

    logout 記述変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,8 @@
 'use client';
 import React from 'react';
-import { supabase } from '@/utils/supabase';
-import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { fetchUserInfo } from "@/app/actions";
+import { logout } from '@/app/actions';
 import '../styles/components/header.css';
 
 export default function Header () {
@@ -20,28 +19,13 @@ export default function Header () {
     }, []); 
   
     const userId = id;
-    const router = useRouter();
     console.log(userId);
 
-    const Logout = async(e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            const { error: logoutError } = await supabase.auth.signOut();
-            if (logoutError) {
-                throw logoutError;
-            }
-            await router.push('/');
-        } catch {
-            alert('エラーが発生しました')
-        }
-    }
     return (
         <div className="flex justify-between mb-12 border-b border-violet-100 p-4">
         <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
         { userId && (
-            <form onSubmit={Logout}>
-                <button className='logoutButton' type="submit">Log out</button>
-            </form>
+            <button formAction={logout} className='logoutButton'>Log out</button>
         )}
         </div>
     )
