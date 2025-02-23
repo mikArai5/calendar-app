b4a8750279fff5a526f59c70b8f4e262e6285659
荒井美香 <mika8500@yk.commufa.jp>
2025-02-23 19:04:33 +0900

    フォーム追加

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -25,6 +25,9 @@ interface CalendarProps {
 }
 
 const Calendar: React.FC<CalendarProps> = ({ userId }) => {
+    const [ title, setTitle ] = useState<string>("");
+    const [ start, setStart ] = useState<string>("");
+    const [ end, setEnd ] = useState<string>("");
     const [ schedules, setSchedules ] = useState<Schedules>([]);
     // 予定が追加されたタイミングで全ての予定取得
 
@@ -51,26 +54,6 @@ const Calendar: React.FC<CalendarProps> = ({ userId }) => {
             console.error("Error fetching schedules:", err);
         }
     }
-
-    const handleDateSelect= async (args: DateSelectArg) => {
-
-        // 登録した予定・開始日・終了日の入力値を取得して代入
-        const title = prompt("予定のタイトルを入力してください");
-        const start = prompt("予定の開始日を入力してください") as string;
-        const end = prompt("予定の終了日を入力してください") as string;
-        const calendarInstance = args.view.calendar;
-        // ログインしているユーザーのIDを取得して代入
-        const user = await fetchUserInfo();
-        const userId = user?.id as string;
-
-        // 選択状態を解除
-        calendarInstance.unselect();
-
-        // 登録した予定・開始日・終了日・ログインしているユーザーのIDをsupabaseに登録
-        if (title) {
-            await addSchedule(title, start, end, userId);
-        }
-    }
     const router = useRouter();
 
     // 追加・表示した予定を押したら詳細ページに遷移
@@ -79,12 +62,31 @@ const Calendar: React.FC<CalendarProps> = ({ userId }) => {
         router.push( `/events/${slug}`);
     }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (title === "" || start === "" || end === "") return;
+        const user = await fetchUserInfo();
+        const userId = user?.id as string;
+        
+        await addSchedule(title, start, end, userId);
+        setSchedules(schedules);
+        setTitle("");
+        setStart("");
+        setEnd("");
+    }
+
     return (
         <>
-        <form className="inputForm">
+        <form className="inputForm" onSubmit={(e) => handleSubmit(e)}>
             <div className="inputForm__item inputForm__title">
                 <label htmlFor="title">タイトル</label>
-                <input id="title" type="text" className="title" />
+                <input
+                    id="title"
+                    type="text"
+                    className="title"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
             </div>
             <div className="inputForm__date">
                 <div className="inputForm__item">
@@ -93,6 +95,8 @@ const Calendar: React.FC<CalendarProps> = ({ userId }) => {
                         id="start"
                         type="date"
                         className="start"
+                        onChange={(e) => setStart(e.target.value)}
+                        value={start}
                     />
                 </div>
                 <div className="inputForm__item">
@@ -101,6 +105,8 @@ const Calendar: React.FC<CalendarProps> = ({ userId }) => {
                         id="end"
                         type="date"
                         className="end"
+                        onChange={(e) => setEnd(e.target.value)}
+                        value={end}
                     />
                 </div>
             </div>
@@ -116,7 +122,6 @@ const Calendar: React.FC<CalendarProps> = ({ userId }) => {
             }}
             events={schedules}
             selectable={true}
-            select={handleDateSelect}
             eventClick={handleDetail}
             contentHeight={"auto"}
             aspectRatio={1.6}
